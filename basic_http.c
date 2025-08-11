1b9314666c8b223b67911982a6cefbd5a38f51b9
mamtaboade <mamtaborade82@gmail.com>
2025-08-11 12:39:01 +0530

    Task 2 – Change HTML content dynamically

--- basic_http.c
+++ basic_http.c
@@ -4,17 +4,28 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <time.h>
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
 
+void get_client_ip(struct sockaddr_in *address, char *ip_buffer) {
+    inet_ntop(AF_INET, &address->sin_addr, ip_buffer, INET_ADDRSTRLEN);
+}
+
+void get_current_time(char *time_buffer) {
+    time_t now = time(NULL);
+    struct tm *tm_info = localtime(&now);
+    strftime(time_buffer, 26, "%Y-%m-%d %H:%M:%S", tm_info);
+}
+
 int main() {
     int server_fd, client_fd;
     struct sockaddr_in address;
-    char response[] = "HTTP/1.1 200 OK\r\n"
-                      "Content-Type: text/html\r\n\r\n"
-                      "<html><body><h1>Hello from Server!</h1></body></html>";
-    char request[BUFFER_SIZE];
+    char buffer[BUFFER_SIZE];
+    char response[BUFFER_SIZE];
+    char time_buffer[26];
+    char client_ip[INET_ADDRSTRLEN];
     socklen_t addrlen = sizeof(address);
 
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -27,14 +38,27 @@ int main() {
 
     client_fd = accept(server_fd, (struct sockaddr*)&address, &addrlen);
     if (client_fd >= 0) {
-        // TASK: Read the HTTP request
-        ssize_t bytes_read = read(client_fd, request, sizeof(request) - 1);
-        if (bytes_read > 0) {
-            request[bytes_read] = '\0'; // Null-terminate the string
-            printf("Received HTTP request:\n%s\n", request); // Log the request
-        }
+        // Log the HTTP request
+        read(client_fd, buffer, BUFFER_SIZE);
+        printf("Received request:\n%s\n", buffer);
 
-        // Send the response
+        // Get client IP address
+        get_client_ip(&address, client_ip);
+
+        // Get current date/time
+        get_current_time(time_buffer);
+
+        // Create the HTTP response
+        snprintf(response, sizeof(response), 
+                 "HTTP/1.1 200 OK\r\n"
+                 "Content-Type: text/html\r\n\r\n"
+                 "<html><body>"
+                 "<h1>Hello from Server!</h1>"
+                 "<p>Current Date/Time: %s</p>"
+                 "<p>Your IP Address: %s</p>"
+                 "</body></html>", 
+                 time_buffer, client_ip);
+
         write(client_fd, response, strlen(response));
         close(client_fd);
     }
